364239afcbed38040d64c28e0eea02897287ef03
YangYoung <YangYoung2021@163.com>
2022-08-07 23:19:49 +0800

    nacos register commit

--- cloud-demo/order-service/src/main/java/cn/itcast/order/OrderApplication.java
+++ cloud-demo/order-service/src/main/java/cn/itcast/order/OrderApplication.java
@@ -1,5 +1,7 @@
 package cn.itcast.order;
 
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -21,5 +23,14 @@ public class OrderApplication {
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
+
+    /**
+     * 这样配置的方案是基于全局的配置，就是说不管访问哪个服务都是随机的
+     * @return 访问规则实现对象
+     */
+/*    @Bean
+    public IRule randomRule() {
+        return new RandomRule();
+    }*/
 
 }
